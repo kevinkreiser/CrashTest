6402987de6f07996ee912c411c0db34e15b31a52
Kevin Kreiser <kevinkreiser@gmail.com>
2019-01-15 09:21:28 -0500

    first commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,59 @@
+#include <android/log.h>
+#include <jni.h>
+#include <string.h>
+#include <signal.h>
+#include <string>
+
+//globals persisting between calls from javaland
+static JavaVM* vm = NULL;
+static jobject activity = NULL;
+static jmethodID callback = NULL;
+
+//gets called first when a signal is sent to the running pid
+static void signal_handler(int signal, siginfo_t*, void*) {
+    __android_log_print(ANDROID_LOG_ERROR, "Native", "Saw signal %d", signal);
+
+    //get an env so we can call back to java
+    JNIEnv* env;
+    if(vm->AttachCurrentThread(&env, NULL) != JNI_OK)
+        return;
+
+    //call back to java with a message
+    __android_log_print(ANDROID_LOG_ERROR, "Native", "Calling with signal %d", signal);
+    std::string message = "Got signal " + std::to_string(signal);
+    jstring msg = env->NewStringUTF(message.c_str());
+    env->CallVoidMethod(activity, callback, msg);
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_kevinkreiser_crashtest_CrashActivity_sendSegv(JNIEnv*, jobject) {
+    raise(SIGSEGV);
+}
+
+extern "C" JNIEXPORT jboolean JNICALL
+Java_com_kevinkreiser_crashtest_CrashActivity_handleSegv(JNIEnv* env, jobject obj) {
+    //get java hooks we need to make the callback
+    env->GetJavaVM(&vm);
+    activity = env->NewGlobalRef(obj);
+    if (!activity)
+        return false;
+    jclass activity_class = env->GetObjectClass(activity);
+    if (!activity_class)
+        return false;
+    callback = env->GetMethodID(activity_class, "callback", "(Ljava/lang/String;)V");
+    if (!callback)
+        return false;
+
+    //try calling back to java with a message
+    jstring message = env->NewStringUTF("No signal");
+    env->CallVoidMethod(activity, callback, message);
+
+    //register for SIGSEGV
+    struct sigaction action;
+    memset(&action, 0, sizeof(struct sigaction));
+    action.sa_sigaction = signal_handler;
+    action.sa_flags = SA_SIGINFO;
+    sigaction(SIGSEGV, &action, NULL);
+
+    return true;
+}
---
+++ app/src/main/java/com/kevinkreiser/crashtest/CrashActivity.java
@@ -0,0 +1,44 @@
+package com.kevinkreiser.crashtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class CrashActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crash);
+
+        //setup segv handler
+        handleSegv();
+        //cause a segv
+        sendSegv();
+    }
+
+    /**
+     * Sets up signal handler for SIGSEGV which will call the callback function below
+     * @return true if the handler was set
+     */
+    public native boolean handleSegv();
+
+    /**
+     * Raises the SIGSEGV signal which will cause the handler to be called
+     */
+    public native void sendSegv();
+
+    /**
+     * A function that the native code will call back when it receives SIGSEGV
+     * as an illustration it just logs
+     *
+     * @param message  The message coming back from c++
+     */
+    public void callback(String message) {
+        Log.e("CrashActivity", message);
+    }
+}
